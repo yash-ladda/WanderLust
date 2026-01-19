7c02b2c5910896e31484d9e2e5434cd13fb7767c
yash-ladda <yashladda7777@gmail.com>
2026-01-19 20:46:15 +0530

    Update MongoDB connection string to use environment variable

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb+srv://yash-ladda:yashladdaincredible@cluster0.s3cv2yc.mongodb.net/?appName=Cluster0";
+const MONGO_URL = process.env.ATLASDB_URL;
 
 main()
     .then((res) => {
